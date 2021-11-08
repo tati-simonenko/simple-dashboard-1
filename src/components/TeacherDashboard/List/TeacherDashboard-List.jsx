81c2d02148f0d37b6072fb0ec9f6be19df36b320
Tatiana Simonenko <tatyana.simonenko@gmail.com>
2021-11-08 09:05:44 +0300

    pass id

--- src/components/TeacherDashboard/List/TeacherDashboard-List.jsx
+++ src/components/TeacherDashboard/List/TeacherDashboard-List.jsx
@@ -1,7 +1,16 @@
 import { STATUSES } from "../TeacherDashboard.constants";
 import './TeacherDashboard-List.css';
 
-export function List({homeworks}) {
+export function List({ homeworks, onHomeworkStatusChange }) {
+
+    const onStatusChange = (id) => {
+        return (e) => {
+            const status = e.target.value;
+
+            onHomeworkStatusChange({ id, status });
+        }
+    }
+
     return (
         <div className="TeacherDashboard-List">
             {
@@ -17,7 +26,19 @@ export function List({homeworks}) {
                             {homework.lection}
                         </div>
                         <div className="TeacherDashboard-ListItemStatus">
-                            {STATUSES[homework.status]}
+                        <select 
+                            className="TeacherDashboard-Control"
+                            name="lection"
+                            id="lection"
+                            value={homework.status}
+                            onChange={onStatusChange(homework.id)}
+                        >
+                            <option value="">Choose ...</option>
+                            {STATUSES.map(status => (
+                                <option value={status} key={status}>{STATUSES[status]}</option>
+                            ))}
+                        </select>
+                        {STATUSES[homework.status]}
                         </div>
                     </div>
                 ))
